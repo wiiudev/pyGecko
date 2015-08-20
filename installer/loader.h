dc811801dc3917f525058e06e89484704cc4b98c
Marionumber1 <gksharkboy@gmail.com>
2015-08-20 18:32:39 -0400

    Add KERN_ADDRESS_TBL for each version

--- installer/loader.h
+++ installer/loader.h
@@ -5,6 +5,27 @@
 #include "../../../libwiiu/src/socket.h"
 #include "../../../libwiiu/src/types.h"
 
+/* Kernel address table */
+#if VER == 200
+	#define KERN_ADDRESS_TBL		0x0
+#elif VER == 210
+	#define KERN_ADDRESS_TBL		0x0
+#elif VER == 300
+	#define KERN_ADDRESS_TBL		0xFFEB66E4
+#elif VER == 310
+	#define KERN_ADDRESS_TBL		0x0
+#elif VER == 400
+	#define KERN_ADDRESS_TBL		0x0
+#elif VER == 410
+	#define KERN_ADDRESS_TBL		0xffeb902c
+#elif VER == 500
+	#define KERN_ADDRESS_TBL		0xffea9e4c
+#elif VER == 532
+	#define KERN_ADDRESS_TBL		0xFFEAAA10
+#else
+#error "Unsupported Wii U software version"
+#endif
+
 void _start();
 
 void _entryPoint();
